64a2383fcdaa05a28030cdf30f229217764688bf
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2017-07-28 09:49:00 +0200

    Fixed stupid stuff

--- MNIST_GAN.py
+++ MNIST_GAN.py
@@ -2,24 +2,30 @@ from __future__ import print_function
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 
 mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
-
 print(mnist)
+
 
 def fc_layer(x, out_size, activation_function=lambda x: x):
     params = {'W': None, 'b': None}
-
     shape = x.get_shape().as_list()
 
     input_is_vector = len(shape) == 2
     last_dim = shape[-1] if input_is_vector else np.prod(shape[1:])
 
-    params['W'] = tf.get_variable("W", [last_dim, out_size],initializer=tf.random_normal_initializer(stddev=tf.cast(tf.sqrt(1./(out_size*last_dim)), tf.float32)));params['b'] = tf.get_variable("b", [out_size], initializer=tf.constant_initializer(0.0))
+    initializer_W = tf.random_normal_initializer(stddev=tf.cast(tf.sqrt(1./(out_size*last_dim)), tf.float32))
+    params['W'] = tf.get_variable("W", [last_dim, out_size], initializer=initializer_W)
+
+    initializer_b = tf.constant_initializer(0.0)
+    params['b'] = tf.get_variable("b", [out_size], initializer=initializer_b)
 
     out = tf.matmul(x, params['W']) if input_is_vector else tf.matmul(tf.reshape(x, (-1, last_dim)), params['W']) + params['b']
 
     return activation_function(out), params
+
 
 def generator(z):
     with tf.variable_scope("fc1"):
@@ -34,7 +40,8 @@ def generator(z):
 
     return gen_prob, gen_vars
 
-def discriminator(z )  :
+
+def discriminator(z):
     with tf.variable_scope("fc1"):
         disc_fc, params_fc = fc_layer(z, 128, tf.nn.relu)
 
@@ -48,29 +55,9 @@ def discriminator(z )  :
 
     return disc_prob, disc_logit, disc_vars
 
-with tf.variable_scope('G'):
-    Z = tf.placeholder(tf.float32, shape=[None, 100], name='Z')
-    G_sample ,G_vars   = generator(Z  )
-
-with tf.variable_scope('D') as scope:
-    X = tf.placeholder(tf.float32, shape=[None, 28 * 28], name='X')
-
-    D_real, D_logit_real, D_vars = discriminator(X)
-    scope.reuse_variables()
-    D_fake, D_logit_fake, _  = discriminator(G_sample)
-
-D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1. - D_fake)) #TODO: somethigg
-G_loss =  - tf.reduce_mean(tf.log(D_fake))
-
-D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=D_vars )
-G_solver =  tf.train.AdamOptimizer().minimize(G_loss, var_list=G_vars)
-
 
 def sample_Z(m,n):
-    return np.random.uniform(-1.,1.,size=[m,n])
-
-mb_size = 64
-Z_dim = 100
+    return np.random.uniform(-1., 1., size=[m,n])
 
 
 def plot_batch(batch):
@@ -86,72 +73,44 @@ def plot_batch(batch):
             grid[r*img_width:r*img_width + img_width,c*img_height:c*img_height + img_height] = batch[r*num_rows + c]
 
     plt.imshow(grid,interpolation='None', cmap='gray')
-    plt.show()
+    plt.pause(1e-5)
 
 
-import matplotlib.pyplot as plt
+with tf.variable_scope('G'):
+    Z = tf.placeholder(tf.float32, shape=[None, 100], name='Z')
+    G_sample, G_vars = generator(Z)
+
+
+with tf.variable_scope('D') as scope:
+    X = tf.placeholder(tf.float32, shape=[None, 28 * 28], name='X')
+
+    D_real, D_logit_real, D_vars = discriminator(X)
+    scope.reuse_variables()
+    D_fake, D_logit_fake, _  = discriminator(G_sample)
+
+
+D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1. - D_fake)) #TODO: somethigg
+G_loss =  - tf.reduce_mean(tf.log(D_fake))
+
+
+D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=D_vars)
+G_solver =  tf.train.AdamOptimizer().minimize(G_loss, var_list=G_vars)
+
+
+mb_size = 64
+Z_dim = 100
 with tf.Session() as sess:
     init = tf.initialize_all_variables()
     sess.run(init)
     for it in range(1000000):
         print('%s'%str(it))
-        X_mb, _  = mnist.train.next_batch(64)
+        X_mb, _  = mnist.train.next_batch(mb_size)
 
-        _, D_loss_curr = sess.run([D_solver, D_loss ], feed_dict={X:X_mb , Z: sample_Z(mb_size,100)})
-        _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z:sample_Z(mb_size,100)})
+        _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X:X_mb , Z: sample_Z(mb_size, Z_dim)})
+        _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z:sample_Z(mb_size, Z_dim)})
 
         if it % 5000 == 0:
-            img = sess.run([G_sample], feed_dict={X:X_mb , Z: sample_Z(mb_size,100)})
+            img = sess.run([G_sample], feed_dict={X:X_mb, Z: sample_Z(mb_size, Z_dim)})
             print(img)
-            img = img[0].reshape([-1,28,28])
+            img = img[0].reshape([-1, 28, 28])
             plot_batch(img)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
